e0fb56c621893c018d52d5b39ec6e165828ff616
HEITOR SCHWINDEN RACHADEL <heitor_rachadel@estudante.sesisenai.org.br>
2025-03-14 11:01:51 -0300

    first commit

--- jogo2d/script.js
+++ jogo2d/script.js
@@ -1,2 +1,33 @@
 const canvas = document.getElementById('jogoCanvas')
 const ctx = canvas.getContext('2d')
+
+// let posicaoXPersonagem = 0
+// let posicaoYPersonagem = 0
+// let larguraPersonagem = 50
+// let alturaPersonagem = 50
+const personagem = {
+    x: 100,
+    y: 350,
+    largura: 50,
+    altura: 50
+}
+function desenharPersonagem() {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(
+        personagem.x,
+        personagem.y,
+        personagem.largura,
+        personagem.altura)
+}
+function atualizarPersonagem(){
+    personagem.y -= 1
+}
+
+function loop() {
+    desenharPersonagem()
+    atualizarPersonagem()
+    //apagar
+    requestAnimationFrame(loop)
+}
+
+loop()
